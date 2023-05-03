fe9bfb2a22085a79838db76b7394951c1c0ac26a
jaiom <jaiomrola@gmail.com>
2023-05-02 19:28:49 -0500

    hide navbar on scroll

--- docs/main.js
+++ docs/main.js
@@ -14,5 +14,19 @@ $('#nav-toggle').on('click', function() {
 $("#nav-toggle").click(function() {
       $("nav ul").toggle();
     });
-    
-    
+
+
+
+// hide navbar on scrol 
+
+var lastScrollTop;
+var navbar = $('#navigation');
+$(window).on('scroll', function() {
+  var scrollTop = $(window).scrollTop();
+  if (scrollTop > lastScrollTop) {
+    navbar.css('top', '-80px');
+  } else {
+    navbar.css('top', '0');
+  }
+  lastScrollTop = scrollTop;
+});
