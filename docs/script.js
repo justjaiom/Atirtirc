5331c41a9253dc26e1282e70bc54e9a1dda71a25
jaiom <jaiomrola@gmail.com>
2023-05-07 16:40:22 -0500

    js for that

--- docs/script.js
+++ docs/script.js
@@ -14,3 +14,21 @@ window.addEventListener('resize', () => {
         document.body.classList.remove('resize-animation-stopper');
     }, 400);
 });
+
+
+function SendMail() {
+    var params = {
+        from_name: document.getElementById("from_name").value,
+        email_id: document.getElementById("email_id").value,
+        message: document.getElementById("message").value,
+        phone: document.getElementById("phone").value,
+        web_url: document.getElementById("web_url").value,
+
+        web_creation: document.getElementById("web_creation").checked,
+        web_maintenance: document.getElementById("web_maintenance").checked,
+        marketing: document.getElementById("marketing").checked,
+    }
+    emailjs.send("service_qrj6ggp", "template_2bbd5jv", params).then(function (res) {
+        alert("Success!" + res.status);
+    })
+}
